3ce5bd15d42e2ca138b5534e9cefaa0eeef29b6f
Elias Ethan Gheeraert <eliasgheeraert@gmail.com>
2025-09-16 13:26:29 +0800

    sm8250-common: init: Include full realme model and RF version
    Co-authored-by: pjgowtham <pjgowtham@gmail.com>
    Co-authored-by: ermyltsor <lamiimuk@163.com>
    Change-Id: I915ce7204d3d2739d2aa43e0e729f8586bf05257

--- init/init_oplus.cpp
+++ init/init_oplus.cpp
@@ -38,6 +38,42 @@ void OverrideProperty(const char* name, const char* value) {
  * after the original property has been set.
  */
 void vendor_load_properties() {
+    auto prjname = std::stoi(GetProperty("ro.boot.prjname", "0"));
+
+    switch (prjname) {
+        case 19795: // bladerunner CN
+            OverrideProperty("ro.product.product.model", "RMX2071");
+            break;
+        case 20607: // bladerunner_48m CN
+            OverrideProperty("ro.product.product.model", "RMX2072");
+            break;
+        case 19705: // bladerunner EU
+            OverrideProperty("ro.product.product.model", "RMX2075");
+            break;
+        case 19706: // bladerunner IN
+            OverrideProperty("ro.product.product.model", "RMX2076");
+            break;
+        case 21615: // rivena CN
+            OverrideProperty("ro.product.product.model", "RMX3366");
+            break;
+        case 21619: // bitra CN
+        case 136730: // bitra CN (Dragon Ball Edition)
+            OverrideProperty("ro.product.product.model", "RMX3370");
+            break;
+        case 136858: // bitra IN
+        case 136859: // bitra EU
+            OverrideProperty("ro.product.product.model", "RMX3370");
+            break;
+        case 21623: // spartan CN
+            OverrideProperty("ro.product.product.model", "RMX3372");
+            break;
+        case 21732: // spartan IN
+        case 21733: // spartan EU
+            OverrideProperty("ro.product.product.model", "RMX3371");
+            break;
+        default:
+            LOG(ERROR) << "Unexpected project name: " << prjname;
+    }
 
     if (std::string content; ReadFileToString("/proc/devinfo/ddr_type", &content)) {
         OverrideProperty("ro.boot.ddr_type", Split(Trim(content), "\t").back().c_str());
