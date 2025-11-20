12ea33cafba409e9e66fba3ef142b9dc80c5d7c9
Michael Bestas <mkbestas@lineageos.org>
2025-11-21 01:43:15 +0200

    sm8250-common: Depend on android.hardware.graphics.common-V7
    Required for BP3A.
    
    Change-Id: Id071b20aa56d5c2e2717215fa6bb6f16f91198ec

--- extract-files.py
+++ extract-files.py
@@ -97,7 +97,7 @@ blob_fixups: blob_fixups_user_type = {
     'odm/lib64/libpwirissoft.so': blob_fixup()
         .call(blob_fixup_nop_call, 'bl', '_ZN4pxlw18qcomSetDisplayModeEPvii', '_ZN4pxlw15irisSetConfigExEiiPKi@plt'),
     'odm/lib64/libAlgoProcess.so': blob_fixup()
-        .replace_needed('android.hardware.graphics.common-V1-ndk_platform.so', 'android.hardware.graphics.common-V6-ndk.so'),
+        .replace_needed('android.hardware.graphics.common-V1-ndk_platform.so', 'android.hardware.graphics.common-V7-ndk.so'),
     ('odm/lib64/libCOppLceTonemapAPI.so', 'odm/lib64/libYTCommon.so', 'odm/lib64/libaps_frame_registration.so'): blob_fixup()
         .replace_needed('libstdc++.so', 'libstdc++_vendor.so'),
     'odm/lib64/libOGLManager.so': blob_fixup()
